acd969acb4dd6a118a6191d775191a5dd2168522
32tarcan <bahadir.tarcan41@gmail.com>
2022-11-04 22:56:53 +0300

    basic

--- Twittermenti/AppDelegate.swift
+++ Twittermenti/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  Twittermenti
-//
-//  Created by Angela Yu on 17/07/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
 
 import UIKit
 
--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Twittermenti
-//
-//  Created by Angela Yu on 17/07/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
 
 import UIKit
 
